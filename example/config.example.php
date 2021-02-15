8193121dbf2e480b032a4e6ccd3fc6dc26aa2ccb
andreytitov <greatkykyryzo@gmail.com>
2021-02-15 19:06:18 +0700

    Revert "close-api"
    This reverts commit b25ed59e

---
+++ example/config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+$path = realpath(dirname(__FILE__));
+$apiScript = realpath($path . "/../src/webmasterApi.php");
+
+// Get this code on https://oauth.yandex.com/client/new page
+$client_id = '';
+$client_secret = '';
